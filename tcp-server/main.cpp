76a3e3054c86192aa5acec6848b91d0bcb5f1f19
Pochetes <robertomiguel2001@gmail.com>
2021-12-02 14:18:00 -0500

    display and echo message while receiving connection

--- tcp-server/main.cpp
+++ tcp-server/main.cpp
@@ -9,6 +9,8 @@
 
 using namespace std;
 
+#define MAXBUFSIZE 4096
+
 int main() {
     /* create a socket */
     int listening = socket(AF_INET, SOCK_STREAM, 0);
@@ -34,9 +36,54 @@ int main() {
         return -3;
     }
     /* accept a call */
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+    char host[NI_MAXHOST];
+    char svc[NI_MAXSERV];
+
+    int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+    if(clientSocket == -1) {
+        cerr << "problem with client connecting!";
+        return -4;
+    }
     /* close the listening socket */
+    close(listening);
+
+    memset(host, 0, NI_MAXHOST);
+    memset(svc, 0 , NI_MAXSERV);
+
+    int result = getnameinfo((sockaddr*)&client, clientSize, host, NI_MAXHOST, svc, NI_MAXSERV, 0);
+
+    if(result) {
+        cout << host << " connected on " << svc << endl;
+    } else {
+        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+        cout << host << " connected on " << ntohs(client.sin_port) << endl;
+    }
+
     /* while receiving - display & echo message */
+    char buf[MAXBUFSIZE];
+    while(true) {
+        // clear the buffer
+        memset(buf, 0, MAXBUFSIZE);
+        // wait for a message
+        int bytesReceived = recv(clientSocket, buf, MAXBUFSIZE, 0);
+        if(bytesReceived == -1) {
+            cerr << "there was a connection issue" << endl;
+            break;
+        }
+
+        if(!bytesReceived) {
+            cout << "the client disconnected" << endl;
+            break;
+        }
+        // display message
+        cout << "Received: " << string(buf, 0, bytesReceived) << endl;
+        // resend message
+        send(clientSocket, buf, bytesReceived + 1, 0);
+    }
     /* close socket */
+    close(clientSocket);
 
     return 0;
 }
